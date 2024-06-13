9544e29e67472070f68528114492d67330fc38d0
konagawa shingo <konashin215@me.com>
2024-06-13 22:38:00 +0900

    create model

--- db/migrate/20240613124850_create_orders.rb
+++ db/migrate/20240613124850_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[7.0]
   def change
     create_table :orders do |t|
-      t.references :food, null: false, foreign_key: true
-      
+      t.integer :total_price, null: false, default: 0
+
       t.timestamps
     end
   end
